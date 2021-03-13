35d38f53a664594507e92f7a8d26318a56afb5d8
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-03-14 00:02:04 +0300

    13.03.2021 commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './components/category/category.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"", pathMatch:"full", component:ProductComponent},
   {path:"products", component:ProductComponent},
-  {path:"category", component:CategoryComponent}
+  {path:"products/category/:categoryId", component:ProductComponent}
 ];
 
 @NgModule({
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,14 +12,27 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsBycategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
 
   getProducts() {
     this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsBycategory(categoryId:number) {
+    this.productService.getProductsByCategoryId(categoryId).subscribe((response) => {
       this.products = response.data;
       this.dataLoaded = true;
     });
